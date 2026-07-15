a51084eec94c41ae96723716ab7c8739ab8c51c3
Doris Bilic <dorisbilic11@gmail.com>
2026-07-15 16:48:25 +0200

    Add Task entity with Status and Priority enumerations

---
+++ src/main/java/com/kanban_api/domain/enumeration/Priority.java
@@ -0,0 +1,7 @@
+package com.kanban_api.domain.enumeration;
+
+public enum Priority {
+  LOW,
+  MED,
+  HIGH
+}
---
+++ src/main/java/com/kanban_api/domain/enumeration/Status.java
@@ -0,0 +1,7 @@
+package com.kanban_api.domain.enumeration;
+
+public enum Status {
+  TO_DO,
+  IN_PROGRESS,
+  DONE
+}
