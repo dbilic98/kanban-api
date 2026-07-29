281e30a7dcc445a0bf6de9a9ee557880d1e109a0
Doris Bilic <dorisbilic11@gmail.com>
2026-07-29 17:38:51 +0200

    Delete unnecessary imports

--- src/main/java/com/kanban_api/domain/model/Task.java
+++ src/main/java/com/kanban_api/domain/model/Task.java
@@ -9,8 +9,6 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Version;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
