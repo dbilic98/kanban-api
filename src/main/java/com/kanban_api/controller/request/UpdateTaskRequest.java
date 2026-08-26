abfd69627af215135a2b77ba4210421ef505f13e
Doris Bilic <dorisbilic11@gmail.com>
2026-08-26 17:25:06 +0200

    Rename requestTaskDto-s

--- src/main/java/com/kanban_api/controller/request/UpdateTaskRequest.java
+++ src/main/java/com/kanban_api/controller/request/UpdateTaskRequest.java
@@ -5,7 +5,7 @@ import com.kanban_api.domain.enumeration.Status;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
-public record UpdateTaskDto(
+public record UpdateTaskRequest(
 
     @NotBlank(message = "Title is mandatory")
     @Size(min = 2, max = 100, message = "Title must be between 2 and 100 characters")
