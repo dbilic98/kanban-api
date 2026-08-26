a2b2029bf43c46ec318ae6f71385baa2317241f4
Doris Bilic <dorisbilic11@gmail.com>
2026-08-26 17:26:38 +0200

    Add domain/dto

---
+++ src/main/java/com/kanban_api/domain/dto/CreateTaskDto.java
@@ -0,0 +1,21 @@
+package com.kanban_api.domain.dto;
+
+import com.kanban_api.domain.enumeration.Priority;
+import com.kanban_api.domain.enumeration.Status;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public record CreateTaskDto(
+
+    @NotBlank(message = "Title is mandatory")
+    @Size(min = 2, max = 100, message = "Title must be between 2 and 100 characters")
+    String title,
+
+    String description,
+
+    Status status,
+
+    Priority priority
+) {
+
+}
