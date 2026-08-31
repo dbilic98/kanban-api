8e2a74b99545848b4c3faeb81caf1ec3bcfaa83f
Doris Bilic <dorisbilic11@gmail.com>
2026-08-31 16:28:16 +0200

    Add annotation @Repository

--- src/main/java/com/kanban_api/domain/repository/TaskRepository.java
+++ src/main/java/com/kanban_api/domain/repository/TaskRepository.java
@@ -2,7 +2,9 @@ package com.kanban_api.domain.repository;
 
 import com.kanban_api.domain.model.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface TaskRepository extends JpaRepository<Task, Long> {
 
 }
