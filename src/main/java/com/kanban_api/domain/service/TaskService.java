d47d5db039e25ee6dde1a0241418780ff165faa1
Doris Bilic <dorisbilic11@gmail.com>
2026-09-04 16:17:23 +0200

    Add ObjectMapper, jackson dependency and JSON Merge Patch, patchTask method

--- src/main/java/com/kanban_api/domain/service/TaskService.java
+++ src/main/java/com/kanban_api/domain/service/TaskService.java
@@ -14,16 +14,21 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import tools.jackson.databind.JsonNode;
+import tools.jackson.databind.ObjectMapper;
 
 @Service
 public class TaskService {
 
   private final TaskRepository taskRepository;
   private final TaskMapper taskMapper;
+  private final ObjectMapper objectMapper;
 
-  public TaskService(TaskRepository taskRepository, TaskMapper taskMapper) {
+  public TaskService(TaskRepository taskRepository, TaskMapper taskMapper,
+      ObjectMapper objectMapper) {
     this.taskRepository = taskRepository;
     this.taskMapper = taskMapper;
+    this.objectMapper = objectMapper;
   }
 
   public TaskDto createTask(CreateTaskDto createTaskDto) {
@@ -65,6 +70,13 @@ public class TaskService {
   }
 
   //JSON Merge Patch?
+  public TaskDto patchTask(Long id, JsonNode patch) {
+    Task task = taskRepository.findById(id)
+        .orElseThrow(() -> new TaskNotFoundException("Task with ID " + id + " not found"));
+
+    objectMapper.readerForUpdating(task).readValue(patch);
+    return taskMapper.toDto(task);
+  }
 
 
   public void deleteTask(Long id) {
